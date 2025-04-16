2c048be3e0e6357b59062da8c23ad04be2d4c401
Celeste Corbeil [Student] <celeste.corbeil@student.sl.on.ca>
2025-04-16 13:48:26 -0400

    modified:   Final Project/Main.html 	modified:   Final Project/MainScript.js 	modified:   Final Project/styles.css

---
+++ Final Project/MainScript.js
@@ -0,0 +1,5 @@
+/*
+CSS IDEAS
+- Click on the game's image and it opens a pop up which you can then press to lead you to the extended site focused on that game page
+- Art showcase slideshow - has arrows to allow u to see more art
+- Animation focus
